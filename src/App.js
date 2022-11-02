81971e0e6b7fd94eab351452ec6de7496b499ac7
Boris Labianca <boris.labianca@mbp-de-boris-1.home>
2022-11-02 17:50:49 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [boolTop, setBoolTop] = useState(false);
+  const [boolMid, setBoolMid] = useState(false);
+  const [boolBot, setBoolBot] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <i class="fa-solid fa-shuttle-space"></i>
+        <span>Ready To Go</span>
       </header>
+      <main>
+        <div className="buttons">
+          <div className="top-buttons">
+            <button
+              onClick={() => {
+                boolTop === false && setBoolTop(!boolTop);
+              }}
+              className={boolTop === false ? "notPushed" : "pushed"}
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                boolTop === true && setBoolTop(!boolTop);
+              }}
+              className={boolTop === false ? "pushed" : "notPushed"}
+            >
+              OFF
+            </button>
+          </div>
+          <div className="middle-buttons">
+            <button
+              onClick={() => {
+                boolMid === false && setBoolMid(!boolMid);
+              }}
+              className={boolMid === false ? "notPushed" : "pushed"}
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                boolMid === true && setBoolMid(!boolMid);
+              }}
+              className={boolMid === false ? "pushed" : "notPushed"}
+            >
+              OFF
+            </button>
+          </div>
+          <div className="bottom-buttons">
+            <button
+              onClick={() => {
+                boolBot === false && setBoolBot(!boolBot);
+              }}
+              className={boolBot === false ? "notPushed" : "pushed"}
+            >
+              ON
+            </button>
+            <button
+              onClick={() => {
+                boolBot === true && setBoolBot(!boolBot);
+              }}
+              className={boolBot === false ? "pushed" : "notPushed"}
+            >
+              OFF
+            </button>
+          </div>
+        </div>
+        <div className="disp-message">
+          <div
+            className={
+              boolBot === true && boolMid === true && boolTop === true
+                ? "go"
+                : "no-way"
+            }
+          >
+            {boolBot === true && boolMid === true && boolTop === true
+              ? "Go!"
+              : "No way!"}
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>
+          Made with{" "}
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            React
+          </a>{" "}
+          at{" "}
+          <a href="https://www.lereacteur.io/" target="_blank" rel="noreferrer">
+            Le Reacteur
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/boris-labianca-01a52871/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Boris
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
